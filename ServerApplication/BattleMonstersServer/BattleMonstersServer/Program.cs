c8612587a8f84ea46e6bf23d05204125870327c5
Ronja Mattsson <ronja_mattsson@outlook.com>
2021-01-15 16:38:49 +0100

    Server Application with connection - missing ability to send data atm

---
+++ ServerApplication/BattleMonstersServer/BattleMonstersServer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BattleMonstersServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Battle Monsters Server";
+
+            Server.Start(50, 26950); //TODO: Get a random free port.
+
+            Console.ReadKey();
+        }
+    }
+}
